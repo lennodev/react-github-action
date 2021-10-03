2fa9f3a468b74a6f60f1d24e68137c0cf70f30b6
Lenno <lennodev@gmail.com>
2021-10-04 00:55:00 +0800

    update index

--- src/App.js
+++ src/App.js
@@ -7,15 +7,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World! <br />
+          This repository aims to demonstrate the capabilities of GitHub actions. <br />
+          The React application will be deployed to GitHub Pages once <br />
+          the source code committed and pushed to main branch.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/lennodev/react-github-action"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn More
         </a>
       </header>
     </div>
